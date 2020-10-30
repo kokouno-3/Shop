25d747ec109215ed7fcee4df883355952076cfa1
KaitoFujihira <k.f.alamoana@gmail.com>
2020-10-30 10:55:01 +0000

    adminと会員一覧表示作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+   def after_sign_out_path_for(resource)
+    new_admin_session_path # ログアウト後に遷移するpathを設定(admin側)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+  get 'customers/show'
+  get 'customers/edit'
+  get 'customers/update'
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+  namespace :admins do
+  resources :homes, only:[:top]
+  resources :customers, only:[:index, :edit, :show, :update]
+end
   devise_for :users
+  root "admins/homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
