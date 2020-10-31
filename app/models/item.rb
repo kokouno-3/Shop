d4e7e7662be27bc86a310799cfa9215f59495f92
EC2 Default User <shirai0119@gmail.com>
2020-10-31 03:44:00 +0000

    test

---
+++ app/models/item.rb
@@ -0,0 +1,7 @@
+class Item < ApplicationRecord
+
+  belongs_to :genre
+  has_many :order_details, dependent: :destroy
+  has_many :carts, dependent: :destroy
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admins do
+    resources :items, only:[:index, :new, :creste, :show, :edit, :update]
+  end
   root 'homes#top'
   get '/about'=> 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
